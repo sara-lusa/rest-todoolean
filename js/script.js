bc8223dc5c204d4cc3c08b52edd9057ff1de029d
Sara <saracorsob@gmail.com>
2020-07-07 15:46:09 +0200

    Scrittura prima chiamata + trasformazione in funzione di questa

--- js/script.js
+++ js/script.js
@@ -2,6 +2,42 @@
 
 $(document).ready(function() {
 
-
+  getAllItemsSaved();
 
 });
+
+// FUNCTIONS
+// Chiamata Ajax GET dell'API per stampare le attività salvate
+// nel server
+function getAllItemsSaved() {
+  $.ajax(
+    {
+      url: 'http://157.230.17.132:3017/todos/',
+      method: 'GET',
+      success: function(dataResponse) {
+
+        // Utilizzo Handlebars per copiare il template della li
+        var source = $('#list-item-template').html();
+        var template = Handlebars.compile(source);
+
+        // Avvio un ciclo for per leggere tutti gli oggetti presetni nell'API
+        for (var i = 0; i < dataResponse.length; i++) {
+          var singleItem = dataResponse[i];
+
+          // Compilo il template
+          var context = {
+            text: singleItem.text,
+            id: singleItem.id,
+          };
+          var html = template(context);
+
+          // Appendo il template compilato alla lista su html
+          $('#todo-list').append(html);
+        }
+      },
+      error: function(richiesta, stato, errore) {
+        alert('Attenzione, si è verificato un errore: ' + errore);
+      }
+    }
+  );
+};
